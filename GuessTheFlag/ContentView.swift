fba0c241bb0c5a0f0e8b752e9f25fb1f47930ecc
samarkalra <skalra@ex2india.com>
2022-08-08 12:50:52 +0530

    Implement challenges

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
-    @State private var scoreTitle = ""
     @State private var score = 0
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
     @State private var correntAnswer = Int.random(in: 0...2)
+    @State private var currentQuestionNumber = 1
+    private var maxNoOfQuestion = 8
     
     var body: some View {
         ZStack {
@@ -23,53 +24,70 @@ struct ContentView: View {
             ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
             
-            VStack(spacing: 15) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    Text(countries[correntAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
+            
+            
+            VStack(spacing: 10) {
+                Text("Question \(currentQuestionNumber)")
+                    .foregroundColor(.white)
+                    .font(.title2)
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .shadow(radius: 5)
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundColor(.white)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correntAnswer])
+                            .foregroundColor(.white)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
             }
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 20)
-            .background(.ultraThinMaterial)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
         }
-        .alert(scoreTitle, isPresented: $showingScore) {
-             Button("Continue", action: askQuestion)
+        .alert("Result", isPresented: $showingScore) {
+             Button("Restart Quiz", action: restartQuiz)
         } message: {
-            Text("Your score is \(score)")
+            Text("You got \(score) correct out of \(maxNoOfQuestion)")
         }
     }
     
     func flagTapped(_ number: Int) {
         if number == correntAnswer {
-            scoreTitle = "Correct"
             score += 1
-        } else {
-            scoreTitle = "Wrong"
         }
         
-        showingScore = true
+        currentQuestionNumber += 1
+        
+        if(currentQuestionNumber > maxNoOfQuestion) {
+            currentQuestionNumber -= 1
+            showingScore = true
+        } else {
+            askQuestion()
+        }
     }
     
     func askQuestion() {
         countries.shuffle()
         correntAnswer = Int.random(in: 0...2)
+    }
+    
+    func restartQuiz() {
+        currentQuestionNumber = 1
+        askQuestion()
     }
 }
 
